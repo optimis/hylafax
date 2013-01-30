b07db8a2ba08bad5eaec718797ce9d8f73b58d27
James Thullbery <jthullbery@teladoc.com>
2013-01-30 10:38:20 -0500

    updated the tests to include more information about the modem

--- lib/fax/faxstat.rb
+++ lib/fax/faxstat.rb
@@ -1,17 +1,28 @@
 module Fax
   class Faxstat
 
-    attr_accessor :response, :jobs, :running
+    attr_accessor :response, :jobs, :running, :modem
 
     def initialize(options={})
       configure_from_env
       @jobs = {}
+      @modem = {running: false, ready: false, free: false, sending: false, response: nil, name: nil, number: nil}
       raise Fax::ConfigError if Fax.configuration.faxstat_path.nil?
     end
 
     def running?
       update_status
       @running
+    end
+
+    def modem_idle?
+      update_status
+      @modem[:free]
+    end
+
+    def modem_sending?
+      update_status
+      @modem[:sending]
     end
 
     def job( job_id )
@@ -22,24 +33,58 @@ module Fax
     private
     def update_status
       @response = faxstat
-      handle_response
+      parse_response
     end
 
     def faxstat
       `#{Fax.configuration.faxstat_path} -sdl`
     end
 
-    def handle_response
-      tmp = @response.split("\n")
+    def parse_response
+      # Clean string
+      tmp = @response.gsub(/[\r]/, '').split("\n").reject{|x| x.empty?}
+      # binding.pry
+
+      # Set running value and remove
       @running = tmp[0].split(":").last.include?("Running")
-      tmp_jobs = tmp[(1..-1)].each{|job| job.split("\n")}.delete_if{|str| str.empty? || str.eql?("\n")}
-      unless tmp_jobs.empty?
-        key = tmp_jobs[0].split(" ")
-        tmp_jobs[(1..-1)].each do |job|
-          value = job.split(" ")
-          @jobs[value[0].to_i] = Hash[key.zip value]
+      tmp = tmp[(1..-1)]
+
+      unless tmp.empty?
+        # Remove duplicate headers      
+        header = tmp.select{|x| x.include?("JID")}.first
+        tmp = [header] + tmp.reject!{|x| x.include?('JID')}
+         
+        # Handle the Modem string
+        # Then remove it
+        if tmp[1][(0...5)].eql?("Modem")
+          @modem[:response] = tmp[1]
+          parse_modem
+          tmp.reject!{|a| a[(0...5)].eql?("Modem")}
+        end
+
+        # binding.pry
+        
+        tmp_jobs = tmp.each{|job| job.split("\n")}.delete_if{|str| str.empty? || str.eql?("\n")}
+        unless tmp_jobs.empty?
+          key = tmp_jobs[0].split(" ")
+          tmp_jobs[(1..-1)].each do |job|
+            value = job.split(" ")
+            @jobs[value[0].to_i] = Hash[key.zip value]
+          end
         end
       end
+    end
+
+    def parse_modem
+      resp = @modem[:response]
+      resp = resp.split(" ")
+      @modem[:name] = resp[1]
+      @modem[:number] = resp[2]
+      @modem[:sending] = resp[3].eql?("Sending")
+      @modem[:running] = @modem[:sending].eql?(true)
+      @modem[:ready] = (!@modem[:sending].eql?(true))
+      @modem[:free] = resp[3].eql?("Free") || resp[3].eql?("Running")
+
     end
 
     def configure_from_env
--- spec/faxstat_spec.rb
+++ spec/faxstat_spec.rb
@@ -8,13 +8,46 @@ describe Fax::Faxstat do
     ENV['FAXSTAT_PATH'] = '/another/path'
   end
 
-  describe 'running' do
-
+  describe 'hylafax running' do
     it 'checks the status of the HylaFAX scheduler' do
       fake_faxstat_response = "HylaFAX scheduler on dev.teladoc.com: Running\r\n"
       Fax::Faxstat.any_instance.stub(:faxstat).and_return(fake_faxstat_response)
 
       Fax.faxstat.running?.should == true
+    end
+  end
+
+  describe 'modem' do
+    it 'checks the status of the Modem is running and idle' do
+      fake_faxstat_response = "HylaFAX scheduler on teldev05.ct.teladoc.com: Running\r\nModem ttyACM0 (+1.203.602.0291): Running and idle\r\n\nJID Pri S           Owner                                 MailAddr Number       Pages Dials     TTS Status\n9   127 D            root                   jthullbery@teladoc.com 2037691544    1:1   1:12         \r\n8   126 F           admin                           root@localhost 2039873397    0:1   0:12         Job aborted by request\r\n"
+      Fax::Faxstat.any_instance.stub(:faxstat).and_return(fake_faxstat_response)
+
+      Fax.faxstat.modem_idle?.should == true
+      Fax.faxstat.modem_sending?.should == false
+    end
+
+    it 'checks the status of the Modem is waiting to come free' do
+      fake_faxstat_response = "HylaFAX scheduler on teldev05.ct.teladoc.com: Running\r\nModem ttyACM0 (+1.203.602.0291): Waiting for modem to come free\r\n\nJID Pri S           Owner                                 MailAddr Number       Pages Dials     TTS Status\n11  127 R            root                   jthullbery@teladoc.com 2037691544    0:1   0:12         \r\n\nJID Pri S           Owner                                 MailAddr Number       Pages Dials     TTS Status\n9   127 D            root                   jthullbery@teladoc.com 2037691544    1:1   1:12         \r\n10  127 D            root                   jthullbery@teladoc.com 2037691544    1:1   1:12         \r\n8   126 F           admin                           root@localhost 2039873397    0:1   0:12         Job aborted by request\r\n"
+      Fax::Faxstat.any_instance.stub(:faxstat).and_return(fake_faxstat_response)
+
+      Fax.faxstat.modem_idle?.should == false
+      Fax.faxstat.modem_sending?.should == false
+    end
+
+    it 'checks the status of the Modem is Sending a job' do
+      fake_faxstat_response = "HylaFAX scheduler on teldev05.ct.teladoc.com: Running\r\nModem ttyACM0 (+1.203.602.0291): Sending job 10\r\n\nJID Pri S           Owner                                 MailAddr Number       Pages Dials     TTS Status\n10  127 R            root                   jthullbery@teladoc.com 2037691544    0:1   0:12         \r\n\nJID Pri S           Owner                                 MailAddr Number       Pages Dials     TTS Status\n9   127 D            root                   jthullbery@teladoc.com 2037691544    1:1   1:12         \r\n8   126 F           admin                           root@localhost 2039873397    0:1   0:12         Job aborted by request\r\n"
+      Fax::Faxstat.any_instance.stub(:faxstat).and_return(fake_faxstat_response)
+
+      Fax.faxstat.modem_idle?.should == false
+      Fax.faxstat.modem_sending?.should == true
+    end
+
+    it 'checks the status of the Modem is waiting to come ready' do
+      fake_faxstat_response = "HylaFAX scheduler on teldev05.ct.teladoc.com: Running\r\nModem ttyACM0 (+1.203.602.0291): Waiting for modem to come ready\r\n\nJID Pri S           Owner                                 MailAddr Number       Pages Dials     TTS Status\n9   127 D            root                   jthullbery@teladoc.com 2037691544    1:1   1:12         \r\n10  127 D            root                   jthullbery@teladoc.com 2037691544    1:1   1:12         \r\n8   126 F           admin                           root@localhost 2039873397    0:1   0:12         Job aborted by request\r\n"
+      Fax::Faxstat.any_instance.stub(:faxstat).and_return(fake_faxstat_response)
+
+      Fax.faxstat.modem_idle?.should == false
+      Fax.faxstat.modem_sending?.should == false
     end
 
   end
