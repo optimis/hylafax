e4d3094fbcf5d9edfa4fb6e62a075e5789d4a01e
Umang Chouhan <uchouhan@optimiscorp.com>
2013-10-16 01:04:55 +0530

    Adding configuration option for faxstat delimiter.
    The default blankspace delimiter does not always work when
    including the status since the status is also blankspace delimited.

--- lib/hylafax/configuration.rb
+++ lib/hylafax/configuration.rb
@@ -1,6 +1,6 @@
 module Hylafax
   class Configuration
-    attr_accessor :sendfax_path, :sendfax_host, :faxstat_path, :faxrm_path, :faxrm_admin_password, :speed, :cover_sheet
+    attr_accessor :sendfax_path, :sendfax_host, :faxstat_path, :faxstat_delimiter, :faxrm_path, :faxrm_admin_password, :speed, :cover_sheet
 
     def initialize(*args)
       if args[0].is_a?(Hash)
@@ -8,6 +8,7 @@ module Hylafax
         sendfax_path          = args[0]['sendfax_path']
         sendfax_host          = args[0]['sendfax_host']
         faxstat_path          = args[0]['faxstat_path']
+        faxstat_delimiter     = args[0]['faxstat_delimiter']
         faxrm_path            = args[0]['faxrm_path']
         faxrm_admin_password  = args[0]['faxrm_admin_password']
         speed                 = args[0]['speed']
@@ -17,6 +18,7 @@ module Hylafax
       @sendfax_path           = sendfax_path          || ENV['FAX_SENDFAX_PATH']
       @sendfax_host           = sendfax_host          || ENV['FAX_SENDFAX_HOST']
       @faxstat_path           = faxstat_path          || ENV['FAX_FAXSTAT_PATH']
+      @faxstat_delimiter      = faxstat_delimiter     || ENV['FAX_FAXSTAT_DELIMITER']
       @faxrm_path             = faxrm_path            || ENV['FAX_FAXRM_PATH']
       @faxrm_admin_password   = faxrm_admin_password  || ENV['FAX_FAXRM_ADMIN_PASSWORD']
       @speed                  = speed                 || ENV['SPEED']
